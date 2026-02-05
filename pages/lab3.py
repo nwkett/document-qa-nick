09e54b0076f9a62c3f6aa0d049b40bfb3d19a5d4
nwkett <113996906+nwkett@users.noreply.github.com>
2026-02-05 17:04:40 +0000

    chat bot

--- pages/lab3.py
+++ pages/lab3.py
@@ -2,14 +2,41 @@ import streamlit as st
 from openai import OpenAI, AuthenticationError
 
 #initial empty messages
-if messages is not initialized:
-    messages = []
-#add user prompt to messages
-prompt = get_chat_input()
+st.title('Nicks Lab3 Question answering chatbot')
 
-if prompt:
-    messages.add('user', prompt)
-#get LLM response
-    response = call_llm('gpt-5-chat', messages, temperature = 0)
-    messages.add('assistant', response.text)
-    show_chat_output(response.text)
+openAI_model = st.sidebar("Select Model", ('mini', 'regular'))
+
+if openAI_model == 'mini':
+    model_to_use = "gpt-4o-mini"
+else:
+    model_to_use = 'gpt-4o'
+
+if 'client' not in st.session_state:
+    api_key = st.secrets["OPENAI_API_KEY"]
+    st.session_state.client = OpenAI(api_key=api_key)
+
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = \
+        [{"role": 'assistant','content': "How can I help you today?"}]
+    
+for msg in st.session_state.messages:
+    chat_msg = st.chat_message(msg["role"])
+    chat_msg.write(msg["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    client = st.session_state.client
+    stream = client.chat.completions.create(
+        model = model_to_use,
+        messages = st.session_state.messages,
+        stream=True
+    )
+
+    with st.chat_message("assistant"):
+        response = st.write_stream(stream)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
